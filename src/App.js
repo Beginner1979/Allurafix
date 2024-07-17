0cdc51668975afa9f2126bad19ed1dae0aa3be45
Beginner79 <bortolini.leonardo@gmail.com>
2024-07-17 00:02:09 -0300

    Correcion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NewVideo from './pages/NewVideo';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/new-video" component={NewVideo} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/new-video" element={<NewVideo />} />
+      </Routes>
     </Router>
   );
 }
